85940a8220c48844cc8060821df0d5235997313d
Manisha <manisharana02005@gmail.com>
2025-08-29 00:30:27 +0530

    added inProgess and Completed buttons

--- src/app/components/task-list-item/task-list-item.component.ts
+++ src/app/components/task-list-item/task-list-item.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+import { TaskItem } from 'src/app/models/task-item.model';
 
 @Component({
   selector: 'app-task-list-item',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-list-item.component.html',
   styleUrls: ['./task-list-item.component.css']
 })
 export class TaskListItemComponent {
- @Input() text!: string;
+  @Input() taskItem!: TaskItem;
+
+  statusValue: ('Todo' | 'InProgress' | 'Completed')[] = ['Todo', 'InProgress', 'Completed'];
+
+  get availableActions(): ('Todo' | 'InProgress' | 'Completed')[] {
+    return this.statusValue.filter(x => x !== this.taskItem.status);
+  }
 }
