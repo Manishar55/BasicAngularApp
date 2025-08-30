f8d77485e1c45716961594b0e995d2cab3972167
Manisha <manisharana02005@gmail.com>
2025-08-30 11:58:18 +0530

    Completed the Angular App

--- src/app/components/services/task.service.ts
+++ src/app/components/services/task.service.ts
@@ -11,7 +11,22 @@ export class TaskService {
 
   addTask(task: string, status: string) {
     this.tasks.update((prev) => {
-      return [...prev, { task, status}];
-  });
+      return [...prev, { task, status }];
+    });
+  }
+
+  markAsStatus(text: string, updatedStatus: string) {
+    this.tasks.update((existingCollection) => {
+      const findTask = existingCollection.find(x => x.task === text);
+      
+      //removing the old task and adding the updated task
+      if(findTask){
+        return [...existingCollection.filter((x) => x.task !== text), {task:text, status:updatedStatus}];
+      }
+      else{
+        return existingCollection;
+      }
+    })
+
   }
 }
--- src/app/components/task-list-item/task-list-item.component.ts
+++ src/app/components/task-list-item/task-list-item.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TaskItem } from 'src/app/models/task-item.model';
-
+import { TaskService } from '../services/task.service';
+import { inject } from '@angular/core';
 @Component({
   selector: 'app-task-list-item',
   standalone: true,
@@ -12,10 +13,15 @@ import { TaskItem } from 'src/app/models/task-item.model';
 })
 export class TaskListItemComponent {
   @Input() taskItem!: TaskItem;
-
+  
   statusValue: ('Todo' | 'InProgress' | 'Completed')[] = ['Todo', 'InProgress', 'Completed'];
+  taskService = inject(TaskService);
 
   get availableActions(): ('Todo' | 'InProgress' | 'Completed')[] {
     return this.statusValue.filter(x => x !== this.taskItem.status);
+  }
+
+  markAs(text: string, updatedStatus: string) {
+    this.taskService.markAsStatus(text, updatedStatus);
   }
 }
